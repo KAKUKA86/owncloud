a456fa87830b9fc7a3724c711e2877192e6a216e
sky09115 <937695960@qq.com>
2022-09-18 15:49:10 +0800

    test

---
+++ src/main/java/com/example/owncloud/OwncloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.owncloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwncloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwncloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/owncloud/OwncloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.owncloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwncloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
