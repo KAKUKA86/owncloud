52d31a8a4ddcd4f3d10035555be26ff068a5bc96
nkfunction <1471751052@qq.com>
2022-10-27 11:26:21 +0800

    艹

--- src/main/java/com/popcorn/owncloud/controller/FileController.java
+++ src/main/java/com/popcorn/owncloud/controller/FileController.java
@@ -1,67 +1,45 @@
 package com.popcorn.owncloud.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
 
+import java.util.UUID;
+
 @Controller
 public class FileController {
-    @RequestMapping("normalUser/userUploadFile")
-    public String fileUploadPage() {
-        return "normalUser/userUploadFile";
-    }
-
     @RequestMapping("userHomePage")
-    @ResponseBody
-    public String uploadFile(@RequestParam ("filename") MultipartFile file){
-        System.out.println("测试进入方法");
-        if (file.isEmpty()) {
-            return "false";
-        }
-        String fileName = file.getOriginalFilename();
-        long size = file.getSize();
-        System.out.println(fileName + "-->" + size);
-
-        String path = "D:/test";
-        File dest = new File(path + "" + fileName);
-        if (!dest.getParentFile().exists()) {
-            dest.getParentFile().mkdir();
-        }
-        try{
-            file.transferTo(dest);
-            return "true";
-        }catch (IllegalStateException | IOException e) {
-            e.printStackTrace();
-            return "false";
-        }
+    public String uploadFile(Model model) {
+        return "normalUser/userHomePage";
     }
 
-//    @RequestMapping("normalUser/userUploadFile")
-//    @ResponseBody
-//    public String fileUpload(@RequestParam("fileName") MultipartFile file) {
-//        if(file.isEmpty()) {
-//            return "false";
-//        }
-//        String fileName = file.getOriginalFilename();
-//        long size = file.getSize();
-//        System.out.println(fileName + "-->" + size);
-//        String path = "D:/test";
-//        File dest = new File(path+"/"+fileName);
-//        if (!dest.getParentFile().exists()) {
-//            dest.getParentFile().mkdir();
-//        }
-//        try {
-//            file.transferTo(dest);
-//            return "true";
-//        } catch (IllegalStateException | IOException e) {
-//            e.printStackTrace( );
-//            return "false";
-//        }
+    @RequestMapping("/uploadFile")
+    public String fileUpload(MultipartFile[] multipartFiles, Model model) {
+        System.out.println("进入函数");
+        for (MultipartFile file : multipartFiles) {
+            String filename = file.getOriginalFilename();
+            //重命名
+            filename = UUID.randomUUID() + "_" + filename;
+            //指定上传目录
+            String path = "D:/test/";
+            File filePath = new File(path);
+            if (!filePath.exists()) {
+                filePath.mkdirs();
+            }
+            try {
+                file.transferTo(new File(path + filename));
+            } catch (IOException e) {
+                e.printStackTrace();
+                model.addAttribute("status", "上传失败" + e.getMessage());
+            }
+        }
+        model.addAttribute("status", "上传成功");
+        return "/normalUser/userHomePage";
+    }
 }
 
 
